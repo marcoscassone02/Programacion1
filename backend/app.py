7b8ba0ac5ab3287e454890d606a011260acfe57a
emilianomassolin <e.massolin@alumno.um.edu.ar>
2024-04-02 11:15:37 -0300

    pasando importaciones a la carpeta init

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,8 @@
+from main import create_app
 import os
-from flask import Flask
-from dotenv import load_dotenv
-
-load_dotenv()
-
-app = Flask(__name__)
-
+#llamamos a la funcion create_app() que se encuentra en el archivo main/__init__.py
+app=create_app()
+app.app_context().push()    
 if __name__ == '__main__':
     app.run(debug=True,port=os.getenv('PORT'))
 
---
+++ backend/main/__init__.py
@@ -0,0 +1,7 @@
+#inicializa la aplicacion de flask
+from flask import Flask
+from dotenv import load_dotenv
+def create_app():
+ app = Flask(__name__)
+ load_dotenv()
+ return app
