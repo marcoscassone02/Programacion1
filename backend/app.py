35e1d5c1310e6d11593a6fe472a6a53407d0e100
StefanoPalazzo <s.palazzo@alumno.um.edu.ar>
2024-04-09 13:35:37 -0300

    Creación Base de datos

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 #llamamos a la funcion create_app() que se encuentra en el archivo main/__init__.py
 app=create_app()
 app.app_context().push()    
+from main import db
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True,port=os.getenv('PORT'))
-
