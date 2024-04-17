b15a0238c58a5e0cd61f35cac56e889bf4a7c81d
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2024-04-17 12:25:35 -0300

    Notificacion ahora acepta datos del tipo date

--- backend/main/models/notificacion.py
+++ backend/main/models/notificacion.py
@@ -1,15 +1,16 @@
 from .. import db
+from datetime import datetime
 
 class Notificacion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mensaje = db.Column(db.String(100),nullable=False)
-    fecha = db.Column(db.String(100),nullable=False)
+    fecha = db.Column(db.Date,nullable=False)
     #Convertir objeto en JSON
     def to_json(self):
         notificacion_json = {
             'id': self.id,
             'mensaje': str(self.mensaje),
-            'fecha': str(self.fecha)
+            'fecha': datetime.strftime(self.fecha, '%Y-%m-%d')
         }
         return notificacion_json
     
@@ -17,8 +18,10 @@ class Notificacion(db.Model):
     def from_json(notificacion_json):
         id = notificacion_json.get('id')
         mensaje = notificacion_json.get('mensaje')
-        fecha = notificacion_json.get('fecha')
+        fecha_str = notificacion_json.get('fecha')
         
+    # Convertir el string de fecha a objeto datetime
+        fecha = datetime.strptime(fecha_str, '%Y-%m-%d').date()
         
         return Notificacion(id=id,
                     mensaje=mensaje,
