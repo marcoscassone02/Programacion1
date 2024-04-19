cd5efa858c448771f62913c1d0ab5759379e28f3
MxSoldati <m.soldati@alumno.um.edu.ar>
2024-04-19 10:59:45 -0300

    configuracion listo

--- backend/main/models/configuracion.py
+++ backend/main/models/configuracion.py
@@ -7,10 +7,22 @@ class Configuracion(db.Model):
     pais = db.Column(db.String(100),nullable=False)
     #Convertir objeto en JSON
     def to_json(self):
-        usuario_json = {
+        configuracion_json = {
             'id': self.id,
             'visualizacion': str(self.visualizacion),
             'idioma':str(self.idioma),
             'pais': str(self.pais),
         }
-        return usuario_json
+        return configuracion_json 
+    
+    def from_json(configuracion_json):
+        id = configuracion_json.get('id')
+        visualizacion = configuracion_json.get('visualizacion')
+        idioma = configuracion_json.get('idioma')
+        pais = configuracion_json.get('pais')
+         
+        return Configuracion(id=id,
+                    visualizacion=visualizacion,
+                    idioma=idioma,
+                    pais=pais
+                    )
