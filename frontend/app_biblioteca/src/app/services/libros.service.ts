defc023bed1c61f66318ffb4cdfd197057680eab
emilianomassolin <e.massolin@alumno.um.edu.ar>
2024-10-17 13:22:38 -0300

    paginacon catalogos

--- frontend/app_biblioteca/src/app/services/libros.service.ts
+++ frontend/app_biblioteca/src/app/services/libros.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,12 +9,12 @@ export class LibrosService {
   constructor(
     private http: HttpClient
   ) { }
-  getLibros() {
+  getLibros(page: number = 1, perPage: number = 10): Observable<any> {
     let auth_token = localStorage.getItem('token');
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'authorization': `Bearer ${auth_token}`
     });
     const requestOptions = {headers: headers};
-    return this.http.get(this.url+'/libros',requestOptions);
+    return this.http.get(`${this.url}/libros?page=${page}&per_page=${perPage}`, requestOptions);
 }}
